b7aaadbf4f25b7003f72b97235d39941630d76cc
Memetastic-Fellow <30875272+Memetastic-Fellow@users.noreply.github.com>
2017-08-21 09:13:40 -1000

    Added two files
    the insult generator

---
+++ good-kind-insult-generator.js
@@ -0,0 +1,9 @@
+function saySomething() {
+  var insultThing = Math.floor(Math.random() * 3);
+  
+  insultThing[0];
+   
+  var insultBob = ["horse jockey", "dumb foul beast", "your different"];
+  
+  window.alert(insultBob[insultThing]);
+}
